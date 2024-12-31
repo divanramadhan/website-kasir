d97537d758ca3fd87f35febf04887c6dac72514c
Muhammad Ergi Divan Ramadhan <129483985+divanramadhan@users.noreply.github.com>
2024-12-31 13:21:46 +0700

    Database

--- db_kasir.sql
+++ db_kasir.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 06 Jul 2022 pada 11.40
--- Versi server: 10.4.13-MariaDB
--- Versi PHP: 7.4.8
+-- Generation Time: Dec 31, 2024 at 07:20 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `cart`
+-- Table structure for table `cart`
 --
 
 CREATE TABLE `cart` (
@@ -36,12 +36,12 @@ CREATE TABLE `cart` (
   `harga_modal` int(11) NOT NULL,
   `qty` int(11) NOT NULL,
   `subtotal` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `inv`
+-- Table structure for table `inv`
 --
 
 CREATE TABLE `inv` (
@@ -51,12 +51,21 @@ CREATE TABLE `inv` (
   `pembayaran` int(11) NOT NULL,
   `kembalian` int(11) NOT NULL,
   `status` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `inv`
+--
+
+INSERT INTO `inv` (`invid`, `invoice`, `tgl_inv`, `pembayaran`, `kembalian`, `status`) VALUES
+(46, 'AD201224135101', '2024-12-20 06:51:33', 150000, 1000, 'selesai'),
+(47, 'AD311224124714', '2024-12-31 05:47:29', 800000, 55000, 'selesai'),
+(48, 'AD311224131513', '2024-12-31 06:15:33', 800000, 88000, 'selesai');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `laporan`
+-- Table structure for table `laporan`
 --
 
 CREATE TABLE `laporan` (
@@ -68,12 +77,21 @@ CREATE TABLE `laporan` (
   `harga_modal` int(11) NOT NULL,
   `qty` int(11) NOT NULL,
   `subtotal` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `laporan`
+--
+
+INSERT INTO `laporan` (`idlaporan`, `invoice`, `kode_produk`, `nama_produk`, `harga`, `harga_modal`, `qty`, `subtotal`) VALUES
+(46, 'AD201224135101', 'flannel01', 'Rowndvsn Kemeja Flanell Kaist Black Blue', 149000, 100000, 1, 149000),
+(47, 'AD311224124714', 'flannel01', 'Rowndvsn Kemeja Flanell Kaist Black Blue', 149000, 100000, 5, 745000),
+(48, 'AD311224131513', 'kaos02', 'Rowndvsn Kaos Trippoly Green Bay', 89000, 69000, 8, 712000);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `login`
+-- Table structure for table `login`
 --
 
 CREATE TABLE `login` (
@@ -84,19 +102,20 @@ CREATE TABLE `login` (
   `alamat` text NOT NULL,
   `telepon` varchar(15) NOT NULL,
   `logo` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `login`
+-- Dumping data for table `login`
 --
 
 INSERT INTO `login` (`userid`, `username`, `password`, `toko`, `alamat`, `telepon`, `logo`) VALUES
-(1, 'adgrafika', '$2y$10$iCsRopXtlyYc5Qvk.bQF8eCCUXAhwExf7Nma4T4TPJg47SAR6/usu', 'Aplikasi Kasir', 'kp. lukun rt.02/01 ds. cisoka kec. cisoka kab.tangerang - Banten', '089635808393', 'logo-adgrafika.png');
+(1, 'adgrafika', '$2y$10$iCsRopXtlyYc5Qvk.bQF8eCCUXAhwExf7Nma4T4TPJg47SAR6/usu', 'Aplikasi Kasir', 'kp. lukun rt.02/01 ds. cisoka kec. cisoka kab.tangerang - Banten', '089635808393', 'logo-adgrafika.png'),
+(2, 'admin', '$2y$10$y5fH2HSmh3Uay4oOaiB/n.0R5E5kPM5.GVj.5tXC4ojGhuS7miBJ6', 'pti store', 'jalan markisa ii no.63', '088888888', 'logo_kasir.png');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `produk`
+-- Table structure for table `produk`
 --
 
 CREATE TABLE `produk` (
@@ -106,75 +125,92 @@ CREATE TABLE `produk` (
   `harga_modal` int(11) NOT NULL,
   `harga_jual` int(11) NOT NULL,
   `tgl_input` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`idproduk`, `kode_produk`, `nama_produk`, `harga_modal`, `harga_jual`, `tgl_input`) VALUES
+(5, 'flannel01', 'Rowndvsn Kemeja Flanell Kaist Black Blue', 100000, 149000, '2024-12-20 06:27:08'),
+(6, 'flannel02', 'Rowndvsn Kemeja Flanell Cambridge Black Red', 100000, 149000, '2024-12-20 06:27:35'),
+(7, 'tas01', 'Rowndvsn Tas Ransel Aristo', 79000, 99000, '2024-12-20 06:28:59'),
+(8, 'kaos01', 'Rowndvsn Kaos Bree Navy', 69000, 89000, '2024-12-20 06:29:57'),
+(9, 'kaos02', 'Rowndvsn Kaos Trippoly Green Bay', 69000, 89000, '2024-12-20 06:31:40'),
+(10, 'kaos03', 'Rowndvsn Kaos Deadler Putih', 69000, 89000, '2024-12-20 06:31:57'),
+(11, 'kaos04', 'Rowndvsn Kaos Deadler Hitam ', 69000, 89000, '2024-12-20 06:32:23'),
+(12, 'kaos05', 'Rowndvsn Kaos Fluido Black', 69000, 89000, '2024-12-20 06:33:44'),
+(13, 'flannel03', 'Rowndvsn Kemeja Flannel Kandira Black', 100000, 149000, '2024-12-20 06:36:08'),
+(14, 'flannel04', 'Rowndvsn Kemeja Cadisone Black Orange', 100000, 149000, '2024-12-20 06:37:38'),
+(15, 'flannel05', 'Rown Division Kemeja Flanel Science Black', 100000, 149000, '2024-12-20 06:38:07');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `cart`
+-- Indexes for table `cart`
 --
 ALTER TABLE `cart`
   ADD PRIMARY KEY (`idcart`);
 
 --
--- Indeks untuk tabel `inv`
+-- Indexes for table `inv`
 --
 ALTER TABLE `inv`
   ADD PRIMARY KEY (`invid`);
 
 --
--- Indeks untuk tabel `laporan`
+-- Indexes for table `laporan`
 --
 ALTER TABLE `laporan`
   ADD PRIMARY KEY (`idlaporan`);
 
 --
--- Indeks untuk tabel `login`
+-- Indexes for table `login`
 --
 ALTER TABLE `login`
   ADD PRIMARY KEY (`userid`);
 
 --
--- Indeks untuk tabel `produk`
+-- Indexes for table `produk`
 --
 ALTER TABLE `produk`
   ADD PRIMARY KEY (`idproduk`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `cart`
+-- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `idcart` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
+  MODIFY `idcart` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 
 --
--- AUTO_INCREMENT untuk tabel `inv`
+-- AUTO_INCREMENT for table `inv`
 --
 ALTER TABLE `inv`
-  MODIFY `invid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `invid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
--- AUTO_INCREMENT untuk tabel `laporan`
+-- AUTO_INCREMENT for table `laporan`
 --
 ALTER TABLE `laporan`
-  MODIFY `idlaporan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `idlaporan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
--- AUTO_INCREMENT untuk tabel `login`
+-- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `produk`
+-- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `idproduk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idproduk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
